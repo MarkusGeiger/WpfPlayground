8a4d2d4affde9d1423430b54923a9953f09f439e
markus <markus@MARKUSGEIGE4B55>
2022-09-23 21:30:40 +0200

    Projektdateien hinzufügen.

---
+++ WpfPlayground.GUIOnly/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfPlayground.GUIOnly
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      Console.WriteLine("1 Grid clicked!");
+      Debug.WriteLine("2 Grid clicked!");
+      Trace.WriteLine("3 Grid clicked!");
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      Console.WriteLine("1 MainWindow loaded!");
+      Debug.WriteLine("2 MainWindow loaded!");
+      Trace.WriteLine("3 MainWindow loaded!");
+    }
+  }
+}
